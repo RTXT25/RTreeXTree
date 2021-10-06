13a3d7897d8216b58ef924d695cf936260a7060d
RTXT25 <skoolhumanboi@gmail.com>
2021-10-06 16:41:42 +1100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,4 @@
-addLayer("W", {
+addLayer("E", {
     name: "Equipment Points", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "E", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -8,14 +8,13 @@ addLayer("W", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Equipment points", // Name of prestige currency
+    resource: "Equipment Points", // Name of prestige currency
     baseResource: "War Points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
-        if (hasUpgrade('p', 11)) gain = gain.times(2)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -28,10 +27,20 @@ addLayer("W", {
     layerShown(){return true},
     upgrades: {
         11: {
-            title: "Make this whatever you want!",
-            description: "Double your point gain.",
+            title: "Two Factorys",
+            description: "Double your EP gain.",
             cost: new Decimal(1),
             
+        },
+        12: {
+            title: "Adaptive Factorys",
+            description: "Boost your production by the amount of EP you have",
+            cost: new Decimal(2),
+            effect() {
+                return player[this.layer].points.add(1).pow(0.5)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        
         },
     },
 })
